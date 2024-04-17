24fdf63176917715519906be6e8169a6d3bca782
lhyeonnit <lhyeonnit@gmail.com>
2024-04-17 14:32:12 +0900

    MainScreenState

---
+++ lib/screens/main_screens.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class MainScreenState extends StatefulWidget {
+
+  @override
+  _MainScreenStateState createState() => _MainScreenStateState();
+}
+
+class _MainScreenStateState extends State<MainScreenState> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: const Text('MainScreens'),
+      ),
+    );
+  }
+}
